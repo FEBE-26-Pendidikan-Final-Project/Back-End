fcae867b4047065da0d6e4327cd8ba731e934427
yogzsp <yogiyogi.prana43@gmail.com>
2022-11-24 18:28:20 +0800

    Update kelas & beberapa kodingan

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,10 @@ router.post('/register', async (req, res) => {
     //create user
     try {
         const saveUser = await user.save()
-        res.json(saveUser)
+        res.status(200).json({
+            "status":res.statusCode,
+            "message":"Berhasil membuat akun!"
+        })
     }catch(err){
         res.status(400).json({
             status: res.statusCode,
@@ -56,7 +59,7 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({email: req.body.email})
     if(!user) return res.status(400).json({
         status: res.statusCode,
-        message: 'Email Anda Salah!'
+        message: 'Email tidak ditemukan!'
     })
 
     // check password
@@ -68,8 +71,9 @@ router.post('/login', async (req, res) => {
 
     // membuat token menggunkan JWT
     const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY)
-    res.header('auth-token', token).json({
-        token: token
+    res.status(200).header('auth-token', token).json({
+        status: res.statusCode,
+        msg: token
     })
 })
 
--- routes/kelas.js
+++ routes/kelas.js
@@ -1,24 +1,52 @@
 const express = require('express')
 const router = express.Router()
 const Kelas = require('../models/Kelas')
+const jwt = require("jsonwebtoken");
 
 const verifyAdmin = require('./verifyAdmin')
 const verifyToken = require('./verifyToken')
 
+const {createValidation} = require("../configs/validationKelas.js")
+
 
 // CREATE
 router.post('/',verifyAdmin, async (req, res) => {
+    const { error } = createValidation(req.body)
+    if(error) return res.status(400).json({
+        status: res.statusCode,
+        message: error.details[0].message
+    })
+    
+    const token = jwt.sign({token:req.body.nama+req.body.guru}, process.env.SECRET_KEY)
+
     const kelasPost = new Kelas({
         nama: req.body.nama,
         guru: req.body.guru,
-        tokenKelas: req.body.tokenKelas
+        tokenKelas: token
     })
 
     try {
-        const kelas = await kelasPost.save()
-        res.json(kelas)
+        const checkKelas = await Kelas.findOne({
+            nama: kelasPost.nama,
+            guru: kelasPost.guru 
+         });
+         if(checkKelas){
+             res.status(400).json({
+                 status:res.statusCode,
+                 message:"Kelas sudah dibuat sebelumnya!"
+             }).end()
+         }else{
+            const kelas = await kelasPost.save()
+            res.status(200).json({
+                status:res.statusCode,
+                message:"Berhasil membuat kelas!"
+            })
+        }
     }catch(err){
-        res.json({message: err})
+        if(res.statusCode != 400) return res.status(400).json({
+            status:res.statusCode,
+            message:"Gagal membuat kelas!"
+        })
     }
 }),
 
@@ -34,35 +62,80 @@ router.get('/', async (req, res) => {
 
 //get kelas by kelas id
 router.get('/:id', async (req, res) => {
-    const kelas = await Kelas.findById(req.params.id)
-    .then( doc => {
-      if(!doc) {return res.status(404).end();}
-      return res.status(200).json({doc , message: "kelas has been found"});
-    })
-
+    try{
+        const kelas = await Kelas.findById(req.params.id,(err,docs)=>{
+            if(!docs){ 
+                res.status(400).json({
+                    status:res.statusCode,
+                    message:"Kelas tidak ditemukan!"
+                })
+            }else{
+                res.status(200).json({
+                    status:res.statusCode,
+                    message:docs
+                })
+            }
+        })
+    }catch(err){
+        if(res.statusCode != 400) return res.status(400).json({
+                status:res.statusCode,
+                message:"Gagal mencari kelas!"
+            })
+    }
   },
 
 // UPDATE
 router.put('/:id',verifyAdmin, async (req, res) => {
-    try{
-        const kelasUpdate = await Kelas.updateOne({_id: req.params.id}, {
-            nama: req.body.nama,
-            guru: req.body.guru,
-            tokenKelas: req.body.tokenKelas
-        })
-        res.json(kelasUpdate)
+    updateKelas:try{
+        const kelasUpdate = await Kelas.findByIdAndUpdate(req.params.id,{
+                nama: req.body.nama,
+                guru: req.body.guru
+            },
+            (err,docs)=>{
+                if(!docs){ 
+                    res.status(400).json({
+                        status:res.statusCode,
+                        message:"Kelas tidak ditemukan!"
+                    })
+                }else{
+                    res.status(200).json({
+                        status:res.statusCode,
+                        message:"Berhasil mengupdate kelas!"
+                    })
+                }
+            }
+        )
+        
+        break updateKelas
     }catch(err){
-        res.json({message: err})
+        if(res.statusCode != 400) return res.status(400).json({
+                status:res.statusCode,
+                message:"Gagal mengupdate kelas!"
+            })
     }
 }),
 
 // DELETE
 router.delete('/:id',verifyAdmin, async (req, res) => {
     try{
-        const kelasUpdate = await Kelas.deleteOne({_id: req.params.id})
-        res.json(kelasUpdate)
+        const kelasUpdate = await Kelas.deleteOne({_id: req.params.id},(err,docs)=>{
+            if(!docs){ 
+                res.status(400).json({
+                    status:res.statusCode,
+                    message:"Kelas tidak ditemukan!"
+                })
+            }else{
+                res.status(200).json({
+                    status:res.statusCode,
+                    message:"Berhasil menghapus kelas!"
+                })
+            }
+        })
     }catch(err){
-        res.json({message: err})
+        if(res.statusCode != 400) return res.status(400).json({
+            status:res.statusCode,
+            message:"Gagal menghapus kelas"
+        })
     }
 })
 )
